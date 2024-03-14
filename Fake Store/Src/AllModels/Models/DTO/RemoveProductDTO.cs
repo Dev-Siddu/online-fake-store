4b90ac515e302540266423c00ab449a648925cd3
Siddarood <siddarood005@gmail.com>
2024-03-14 18:17:48 +0530

    Delete feature added

--- Fake Store/Src/AllModels/Models/DTO/RemoveProductDTO.cs
+++ Fake Store/Src/AllModels/Models/DTO/RemoveProductDTO.cs
@@ -2,7 +2,7 @@
 {
     public class RemoveProductDTO
     {
-        public int ID { get; set; }
+        public long ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string ImagePath { get; set; }
--- Fake Store/Src/UI/FakeStore.UI/Controllers/ProductsController.cs
+++ Fake Store/Src/UI/FakeStore.UI/Controllers/ProductsController.cs
@@ -58,12 +58,12 @@ namespace FakeStore.UI.Controllers
         public async Task<IActionResult> AddNewProd(AddProductDTO addProd)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
-            
+
             HttpClient client = _httpClientFactory.CreateClient(); // HttpClient Created
 
             // Formatting the content of model
             addProd.Name = addProd.Name.Trim().Replace(" ", "");
-            addProd.Description = addProd.Description.Trim().Replace(" ","");
+            addProd.Description = addProd.Description.Trim().Replace(" ", "");
 
             var formData = new MultipartFormDataContent();
             formData.Add(new StringContent(addProd.Name, System.Text.Encoding.UTF8), "Name");
@@ -71,8 +71,8 @@ namespace FakeStore.UI.Controllers
             formData.Add(new StringContent(addProd.Price.ToString().Trim(), System.Text.Encoding.UTF8), "Price");
             formData.Add(new StreamContent(addProd.ImageFile.OpenReadStream()), "ImageFile", addProd.ImageFile.FileName);
             HttpResponseMessage response = await client.PostAsync("http://localhost:5035/api/FakeStore/AddProduct", formData);
-            if (!response.IsSuccessStatusCode) 
-                {
+            if (!response.IsSuccessStatusCode)
+            {
                 ViewBag.Response = 0;
                 return View(new AddProductDTO());
             };
@@ -98,12 +98,69 @@ namespace FakeStore.UI.Controllers
             return View(products);
         }
 
-        [HttpDelete]
+        [HttpGet]
         [Route("[Action]")]
-        public async Task<IActionResult> RemoveProduct([FromQuery]RemoveProductDTO prod)
+        public async Task<IActionResult> RemovingProductCompleteDetails(RemoveProductDTO prod)
         {
-            Console.WriteLine("Hello");
-            return View();
+            List<Product> prods = await GetProductsAsync();
+            Product? gotProd = prods.FirstOrDefault(temp =>
+            {
+                if(temp.ID == prod.ID && temp.Name == prod.Name && temp.Price == prod.Price && temp.ImagePath == prod.ImagePath)
+                {
+                    return true;
+                }
+                return false;
+            });
+            if (gotProd == null) return NotFound();
+
+            return View(gotProd);
+        }
+
+        [HttpPost]
+        [Route("[Action]")]
+        public async Task<IActionResult> FinallyRemoveProduct(Product prod)
+        {
+            RemoveProductDTO rProd = new RemoveProductDTO()
+            {
+                ID = prod.ID,
+                Name = prod.Name,
+                Price = prod.Price,
+                ImagePath = prod.ImagePath
+            };
+
+            string jsonContent = JsonConvert.SerializeObject(rProd);
+            HttpContent content = new StringContent(jsonContent,System.Text.Encoding.UTF8,"application/json");
+
+            HttpClient client = _httpClientFactory.CreateClient();
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri("http://localhost:5035/api/FakeStore/RemoveProduct"),
+                Content = content
+            };
+            HttpResponseMessage response = await client.SendAsync(requestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok();
+            }
+            return StatusCode(500);
+        }
+
+        [NonAction]
+        public async Task<List<Product>?> GetProductsAsync()
+        {
+            HttpClient client = _httpClientFactory.CreateClient(); // HttpClient created.
+            HttpRequestMessage requestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("http://localhost:5035/api/FakeStore/Products")
+            };
+            HttpResponseMessage response = await client.SendAsync(requestMessage);
+            string content = await response.Content.ReadAsStringAsync();
+            //var products = JsonSerializer.Deserialize<List<Product>>(content);
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(content);
+            return products;
         }
     }
 }
